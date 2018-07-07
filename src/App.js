190a7d11c01de6b202f5f4801d7257b64f9e0c7c
KatietheKoder <katieknecht@ymail.com>
2018-07-07 12:09:41 -0400

    Rounting-jams4

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
        </header>
        <main>
        <Route exact path="/" component={Landing} />
-       <Route path="/Library" component={Library} />
+       <Route path="/library" component={Library} />
        <Route path="/album/:slug" component={Album} />
        </main>
       </div>
