52704eb818aebf4f41d5c5017ccd009685a639f4
KatietheKoder <katieknecht@ymail.com>
2018-07-08 18:31:01 -0400

    error2

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
   class Album extends Component {
+  constructor(props) {
+       super(props);
+
+       const album = albumData.find( album => {
+       return album.slug === this.props.match.params.slug
+     });
+
+     this.state = {
+       album: album
+     };
+     }
+
      render() {
        return (
          <section className="album">
-           {this.props.match.params.slug} Album will go here
+         <section id="album-info">
+           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className="album-details">
+          <h1 id="album-title">{this.state.album.title}</h1>
+          <h2 className="artist">{this.state.album.artist}</h2>
+          <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+          render() {
+            return (
+              <section className="album">
+                {
+                  this.state.album.songs.map( (album, index) =>
+                  <Link to={`/album/${album.slug}`} key={index}>
+                     <img src={album.albumCover} alt={album.title} />
+                     <div>{album.title}</div>
+                     <div>{album.artist}</div>
+                     <div>{album.songs.length} songs</div>
+                     </Link>
+                  )
+                }
+              </section>
+            );
+          }
+        }
+          </tbody>
+        </table>
          </section>
        );
      }
