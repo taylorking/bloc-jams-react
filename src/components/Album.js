2dc4a26c4d1fd6a1b77f63c91c342ea942bee551
KatietheKoder <katieknecht@ymail.com>
2018-07-23 19:58:57 -0400

    error5

--- src/components/Album.js
+++ src/components/Album.js
@@ -72,36 +72,34 @@ import Ionicon from 'react-ionicons';
 
      render() {
        return (
-         <section className="album">
-         <section id="album-info">
-         <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-          <div className="album-details">
-           <h1 id="album-title">{this.state.album.title}</h1>
-           <h2 className="artist">{this.state.album.artist}</h2>
-          <div id="release-info">{this.state.album.releaseInfo}</div>
+   <section className="album">
+      <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+            <div className="album-details">
+              <h1 id="album-title">{this.state.album.title}</h1>
+              <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
-        </section>
-        <table id="song-list">
-         <colgroup>
-           <col id="song-number-column" />
-           <col id="song-title-column" />
-           <col id="song-duration-column" />
-         </colgroup>
-         <section>
-         <table>
-         <tbody>
-         this.state.album.songs.map((song, index) =>
-             <tr> className="song" key={index} onClick={() => this.handleSongClick(song)}>
-             <td onMouseEnter={() => this.onHover(song)} onMouseLeave={() => this.offHover(song)}></td>
-             <td> <ion-icon name="play"></ion-icon></td>
-             <td> <ion-icon name="pause"></ion-icon></td>
-             <td> {song.title}</td>
-             <td> {song.duration}</td>
-             </tr>
-           )}
-         </tbody>
-        </table>
       </section>
+    <table id="song-list">
+      <colgroup>
+         <col id="song-number-column" />
+         <col id="song-title-column" />
+         <col id="song-duration-column" />
+      </colgroup>
+      <tbody>
+      this.state.album.songs.map((song, index) => {
+        <tr> className="song" key={index} onClick={() => this.handleSongClick(song)}>
+        <td onMouseEnter={() => this.onHover(song)} onMouseLeave={() => this.offHover(song)}></td>
+        <td> <ion-icon name="play"></ion-icon></td>
+        <td> <ion-icon name="pause"></ion-icon></td>
+        <td> {song.title}</td>
+        <td> {song.duration}</td>
+        </tr>
+        )}
+      </tbody>
+    </table>
+    </section>
     );
   }
 }
